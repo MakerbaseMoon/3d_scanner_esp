33e892010127df6dbab53ecb25eecd68c19f3dac
Hsun <hsun1031@outlook.com>
2024-08-21 02:56:59 +0800

    🐛 fix: NNS data len too long

--- include/components/data.h
+++ include/components/data.h
@@ -13,8 +13,8 @@
 
 // STA WiFi
 
-#define NVS_STA_SSID             "sta_ssid"
-#define NVS_STA_PASSWORD         "sta_password"
+#define NVS_STA_SSID             "SS"
+#define NVS_STA_PASSWORD         "SP"
 #ifdef CONFIG_ESP_WIFI_SSID
 #define NVS_STA_SSID_DEFAULT     CONFIG_ESP_WIFI_SSID
 #else
@@ -28,8 +28,8 @@
 
 // AP WiFi
 
-#define NVS_AP_SSID             "ap_ssid"
-#define NVS_AP_PASSWORD         "ap_password"
+#define NVS_AP_SSID             "AS"
+#define NVS_AP_PASSWORD         "AP"
 #ifdef CONFIG_ESP_WIFI_AP_SSID
 #define NVS_AP_SSID_DEFAULT     CONFIG_ESP_WIFI_AP_SSID
 #else
@@ -43,39 +43,39 @@
 
 // mDNS
 
-#define NVS_MDNS_HOSTNAME         "mdns"
+#define NVS_MDNS_HOSTNAME         "M"
 #define NVS_MDNS_HOSTNAME_DEFAULT "3d-scanner"
 
 // Github
-#define NVS_GITHUB_REPO             "github_repo"
-#define NVS_GITHUB_USERNAME         "github_username"
+#define NVS_GITHUB_REPO             "GR"
+#define NVS_GITHUB_USERNAME         "GU"
 #define NVS_GITHUB_REPO_DEFAULT     "3d_scanner_esp"
 #define NVS_GITHUB_USERNAME_DEFAULT "MakerbaseMoon"
 
 // Module
 
-#define NVS_Z_AXIS_MAX           "z_axis_max"
-#define NVS_Z_AXIS_START_STEP    "z_axis_start_step"
-#define NVS_Z_AXIS_DELAY_TIME    "z_axis_delay_time"
-#define NVS_Z_AXIS_ONE_TIME_STEP "z_axis_one_time_step"
+#define NVS_Z_AXIS_MAX           "ZM"
+#define NVS_Z_AXIS_START_STEP    "ZSS"
+#define NVS_Z_AXIS_DELAY_TIME    "ZDT"
+#define NVS_Z_AXIS_ONE_TIME_STEP "ZOTS"
 
 #define NVS_Z_AXIS_MAX_DEFAULT         47000
 #define NVS_Z_AXIS_START_STEP_DEFAULT      0
 #define NVS_Z_AXIS_DELAY_TIME_DEFAULT    100
 #define NVS_Z_AXIS_ONE_TIME_STEP_DEFAULT 100
 
-#define NVS_X_Y_AXIS_MAX             "x_y_axis_max"
-#define NVS_X_Y_AXIS_CHECK_TIMES     "x_y_axis_check_times"
-#define NVS_X_Y_AXIS_STEP_DELAY_TIME "x_y_axis_step_delay_time"
-#define NVS_X_Y_AXIS_ONE_TIME_STEP   "x_y_axis_one_time_step"
+#define NVS_X_Y_AXIS_MAX             "2M"
+#define NVS_X_Y_AXIS_CHECK_TIMES     "2CT"
+#define NVS_X_Y_AXIS_STEP_DELAY_TIME "2STD"
+#define NVS_X_Y_AXIS_ONE_TIME_STEP   "2OTS"
 
 #define NVS_X_Y_AXIS_MAX_DEFAULT             6400
 #define NVS_X_Y_AXIS_CHECK_TIMES_DEFAULT       20
 #define NVS_X_Y_AXIS_STEP_DELAY_TIME_DEFAULT  100
 #define NVS_X_Y_AXIS_ONE_TIME_STEP_DEFAULT     16
 
-#define NVS_VL53L1X_CENTER         "vl53l1x_center"
-#define NVS_VL53L1X_TIMEING_BUDGET "vl53l1x_timeing_budget"
+#define NVS_VL53L1X_CENTER         "R_C"
+#define NVS_VL53L1X_TIMEING_BUDGET "R_TB"
 
 #define NVS_VL53L1X_CENTER_DEFAULT            70
 // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms!
